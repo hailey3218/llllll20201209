5ecffb7c505ff87969ac4360e78d12045e3d44c5
Hyehyeon Bang <hbang@c5r5s1.42seoul.kr>
2020-10-30 17:59:59 +0900

    1

---
+++ ft_strlcat.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbang <hbang@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/20 15:23:32 by hbang             #+#    #+#             */
+/*   Updated: 2020/10/30 17:05:45 by hbang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int		ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (*dest && i < size)
+	{
+		++i;
+		++dest;
+	}
+	while (*src && i + 1 < size)
+	{
+		*dest = *src;
+		++dest;
+		++src;
+		++i;
+	}
+	if (i < size)
+		*dest = 0;
+	while (*src)
+	{
+		++i;
+		++src;
+	}
+	return (i);
+}
---
+++ ft_strlcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbang <hbang@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/20 15:22:03 by hbang             #+#    #+#             */
+/*   Updated: 2020/10/20 15:22:06 by hbang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int i;
+	unsigned int source_len;
+
+	i = 0;
+	source_len = 0;
+	while (src[source_len] != '\0')
+	{
+		source_len++;
+	}
+	if (size == 0)
+	{
+		return (source_len);
+	}
+	while (src[i] != '\0' && i + 1 < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (source_len);
+}

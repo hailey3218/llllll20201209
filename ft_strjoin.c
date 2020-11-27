ff81004076c660145d16557d3e64f2afa8ce6f17
Hyehyeon Bang <hbang@c1r7s1.42seoul.kr>
2020-11-27 18:37:53 +0900

    2

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hbang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 17:07:20 by hbang             #+#    #+#             */
-/*   Updated: 2020/11/27 18:15:25 by hbang            ###   ########.fr       */
+/*   Updated: 2020/11/27 18:37:19 by hbang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,15 @@ char		*ft_strjoin(char const *s1, char const *s2)
 	l1 = ft_strlen(s1);
 	l2 = ft_strlen(s2);
 	dst = (char *)malloc(sizeof(char) * (l1 + l2 + 1));
-	if (!(s1) && !(s2))
+	if (!(s1) || !(s2))
+		return (0);
+	if (dst == '\0')
 		return (0);
 	else if (s1 != '\0' && s2 != '\0')
 	{
-		memcpy(dst, s1, l1);
-		memcpy(dst + l1, s2, l1 + l2);
-		dst[l1 + l2 + 1] = '\0';
+		ft_memcpy(dst, s1, l1);
+		ft_memcpy(dst + l1, s2, l1 + l2);
+		dst[l1 + l2] = '\0';
 	}
 	return (dst);
 }

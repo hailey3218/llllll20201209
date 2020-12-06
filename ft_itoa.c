4b09f6e09cd1249a0bbd80b0ade2f1e38f76d7f0
Hyehyeon Bang <hbang@c1r5s3.42seoul.kr>
2020-12-07 01:14:16 +0900

    2

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,22 +6,22 @@
 /*   By: hbang <hbang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/06 23:10:05 by hbang             #+#    #+#             */
-/*   Updated: 2020/12/07 00:59:38 by hbang            ###   ########.fr       */
+/*   Updated: 2020/12/07 01:12:03 by hbang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-long int	check_len(long int n)
+long int		check_len(long int n)
 {
-	int		i;
+	long int	i;
 
 	i = 0;
-	if (n == 0)
+	if (n <= 0)
 		i = 1;
 	if (n < 0)
-		i = 1;
-	while (n != 0)
+		n = n * -1;
+	while (n > 0)
 	{
 		n = n / 10;
 		i++;
@@ -29,23 +29,28 @@ long int	check_len(long int n)
 	return (i);
 }
 
-long int	check_minus(long int n)
+long int		check_minus(long int n)
 {
+	long int	new;
+
 	if (n < 0)
-		n = n * -1;
+	{	
+		new = n * -1;
+		return (new);
+	}
 	return (n);
 }
 
-char		*ft_itoa(int n)
+char			*ft_itoa(int n)
 {
-	char	*str;
-	int		len;
-	int		minus;
+	char		*str;
+	int			len;
+	int			sign;
 
 	if (n < 0)
-		minus = -1;
-	n = check_minus(n);
+		sign = -1;
 	len = check_len(n);
+	n = check_minus(n);
 	str = (char *)malloc(sizeof(char) * len + 1);
 	if (str == NULL)
 		return (0);
@@ -57,7 +62,7 @@ char		*ft_itoa(int n)
 		n = n / 10;
 		len--;
 	}
-	if (minus == -1)
+	if (sign == -1)
 		str[0] = '-';
 	return (str);
 }

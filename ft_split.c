6aa16b158201001eaf8081e1a74339814570989c
Hyehyeon Bang <hbang@c1r7s7.42seoul.kr>
2020-12-12 22:11:22 +0900

    2

--- ft_split.c
+++ ft_split.c
@@ -6,107 +6,86 @@
 /*   By: hbang <hbang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 20:51:09 by hbang             #+#    #+#             */
-/*   Updated: 2020/12/12 03:42:38 by hbang            ###   ########.fr       */
+/*   Updated: 2020/12/12 22:10:59 by hbang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count(char const *s, char c)
+void		**my_free(char **target)
 {
-	int		i;
-	int		count;
+	size_t	i;
 
 	i = 0;
-	count = 0;
-	while (s[i])
+	while (target[i])
 	{
-		if (s[i] && s[i] != c)
-		{
-			count++;
-			while (s[i] && s[i] != c)
-				i++;
-		}
-		else if (s[i] && s[i] == c)
+		free(target[i]);
+		target[i] = NULL;
+		i++;
+	}
+	free(target);
+	target = NULL;
+	return (NULL);
+}
+
+int			count(char const *s, char c)
+{
+	int		count;
+	size_t	i;
+	int		have;
+
+	count = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		have = 0;
+		while (s[i] != '\0' && s[i] == c)
 			i++;
+		while (s[i] != '\0' && s[i] != c)
+		{
+			have = 1;
+			i++;
+		}
+		if (have == 1)
+			count++;
 	}
 	return (count);
 }
 
-static int	count_len(char const *s, char c, int i)
+int			check_len(char const *s, char c)
 {
-	int		length;
+	int		len;
 
-	length = 0;
-	if (s[i] == c)
-		i++;
-	while (s[i] != c && s[i] != '\0')
-	{
-		length++;
-		i++;
-	}
-	return (length);
-}
-
-static void	**my_free(char **new_s, int cnt)
-{
-	int		i;
-
-	i = 0;
-	while (i < cnt)
-	{
-		if (new_s[i])
-		{
-			free(new_s[i]);
-			new_s[i] = NULL;
-		}
-		i++;
-	}
-	free(new_s);
-	new_s = NULL;
-	return (NULL);
-}
-
-static char	**my_split(char const *s, char c, char **new_s, int cnt)
-{
-	int		i[3];
-
-	i[0] = 0;
-	i[1] = 0;
-	while (s[i[1]] != '\0')
-	{
-		i[2] = 0;
-		while (s[i[1]] != '\0' && s[i[1]] == c)
-			i[1]++;
-		if (s[i[1]] != '\0' && s[i[1]] != c)
-		{
-			if (!(new_s[i[0]] = (char *)malloc(sizeof(char) *
-							((count_len(s, c, i[1])) + 1))))
-			{
-				my_free(new_s, cnt);
-				return (NULL);
-			}
-			while (s[i[1]] != '\0' && s[i[1]] != c)
-				new_s[i[0]][i[2]++] = s[i[1]++];
-			new_s[i[0]++][i[2]] = '\0';
-		}
-	}
-	new_s[i[0]] = NULL;
-	return (new_s);
+	len = 0;
+	while (s[len] != '\0' && s[len] != c)
+		len++;
+	return (len);
 }
 
 char		**ft_split(char const *s, char c)
 {
-	size_t	cnt;
 	char	**new_s;
-	int		j;
+	size_t	i;
+	size_t	j;
+	size_t	length;
 
-	j = 0;
 	if (!s)
-		return (0);
-	cnt = count(s, c);
-	new_s = (char **)malloc(sizeof(char *) * (cnt + 1));
-	if (!new_s)
-		return (0);
-	return (my_split(s, c, new_s, cnt));
+		return (NULL);
+	if (!(new_s = (char **)malloc(sizeof(char *) * (count(s, c) + 1))))
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] != '\0' && s[i] == c)
+			i++;
+		if (!(length = check_len(s + i, c)))
+			break ;
+		if (!(new_s[j] = (char *)malloc(sizeof(char) * (length + 1))))
+			return ((char **)my_free(new_s));
+		ft_strlcpy(new_s[j++], s + i, length + 1);
+		i = i + length;
+	}
+	new_s[j] = NULL;
+	return (new_s);
 }
